6ae0fbc96074695b83459f00c9f89301eeb013df
jalofernandez <jalofernandez@gmail.com>
2021-03-22 11:13:48 +0100

    ⬆️  Project version updated to 2.0.4

--- store/owner.js
+++ store/owner.js
@@ -9,7 +9,7 @@ export const state = () => ({
   url: url,
   email: email,
   copyright: author,
-  version: '2.0.4',
+  version: '2.0.5',
   schema: {
     '@context': 'http://schema.org',
     '@type': 'LocalBusiness',
